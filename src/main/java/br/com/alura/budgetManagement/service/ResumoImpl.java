dd5593a250e4e63ed5d61afc331537437e3e76b4
Luiz Gustavo Silva D <lgsduran@yahoo.com.br>
2022-01-27 18:21:01 -0300

    impl

--- src/main/java/br/com/alura/budgetManagement/service/ResumoImpl.java
+++ src/main/java/br/com/alura/budgetManagement/service/ResumoImpl.java
@@ -1,11 +1,16 @@
 package br.com.alura.budgetManagement.service;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import br.com.alura.budgetManagement.entity.Despesas;
 import br.com.alura.budgetManagement.entity.Receitas;
+import br.com.alura.budgetManagement.enums.CategoriaType;
 import br.com.alura.budgetManagement.exception.BusinessException;
 import br.com.alura.budgetManagement.helpers.SupplierHelp;
 import br.com.alura.budgetManagement.repository.DespesasRepository;
@@ -44,13 +49,14 @@ public class ResumoImpl implements IResumo {
 		
 		double receitaTotal = receitas.stream().mapToDouble(x -> x.getValor()).sum();
 		double despesaTotal = despesas.stream().mapToDouble(x -> x.getValor()).sum();
+		Map<CategoriaType, Double> categorias = despesas.stream().collect(
+				Collectors.groupingBy(Despesas::getCategoria, 
+						Collectors.summingDouble(Despesas::getValor)));
 		
 		resumo.setValor_total_das_receitas_no_mes(receitaTotal);
 		resumo.setValor_total_das_despesas_no_mes(despesaTotal);
-		resumo.setSaldo_final_no_mes(receitaTotal - despesaTotal);
-		
-		for (Despesas despesa : despesas)
-			resumo.getCategoria().put(despesa.getCategoria(), despesa.getValor());	
+		resumo.setSaldo_final_no_mes(receitaTotal - despesaTotal);		
+		resumo.setCategoria(categorias);
 		
 		return resumo;
 	}
