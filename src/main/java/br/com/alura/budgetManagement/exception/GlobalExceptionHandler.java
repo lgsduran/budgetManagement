9192a683d59bad2306912753ffc0122de21e4fbd
Luiz Gustavo Silva D <lgsduran@yahoo.com.br>
2022-01-28 10:15:38 -0300

    impl

--- src/main/java/br/com/alura/budgetManagement/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/alura/budgetManagement/exception/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package br.com.alura.budgetManagement.exception;
 
-import static org.springframework.http.HttpStatus.*;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY;
 
 import javax.validation.UnexpectedTypeException;
 
@@ -17,28 +19,34 @@ import lombok.extern.log4j.Log4j2;
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(BusinessException.class)
-	public ResponseEntity<?> BusinessException(BusinessException ex) {
+	public ResponseEntity<?> handleBusinessException(BusinessException ex) {
 		Response response = new Response("422", ex.getMessage());
 		log.error("UNPROCESSABLE_ENTITY: {}", ex.getMessage());
 		return new ResponseEntity<>(response, UNPROCESSABLE_ENTITY);
 	}
 
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> getMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+	public ResponseEntity<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
 		Response response = new Response("404", ex.getBindingResult().getAllErrors().get(0).getDefaultMessage());
 		log.error("Bad request: {}", ex.getMessage());
 		return new ResponseEntity<>(response, BAD_REQUEST);
 	}
 	
 	@ExceptionHandler(UnexpectedTypeException.class)
-	public ResponseEntity<?> getUnexpectedTypeException(UnexpectedTypeException ex) {
+	public ResponseEntity<?> handleUnexpectedTypeException(UnexpectedTypeException ex) {
 		Response response = new Response("404", ex.getMessage());
 		log.error("Bad request: {}", ex.getMessage());
 		return new ResponseEntity<>(response, BAD_REQUEST);
 	}
 
 	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> globleExcpetionHandler(Exception ex) {
+	public ResponseEntity<?> handleGlobleExcpetionHandler(Exception ex) {		
+		if (ex.getMessage().contains("br.com.alura.budgetManagement.enums")) {
+			Response response = new Response("404","Invalid input.");
+			log.error("Bad request: {}", ex.getMessage());
+			return new ResponseEntity<>(response, BAD_REQUEST);
+		}
+		
 		Response response = new Response("500",ex.getMessage());
 		log.error("Internal server error: {}", ex.getMessage());
 		return new ResponseEntity<>(response, INTERNAL_SERVER_ERROR);
